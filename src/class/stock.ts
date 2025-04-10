a934967b2cb4ee9a69829ea63fffb397ba7e6534
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-04-09 23:17:42 -0400

    add stockData

--- src/class/stock.ts
+++ src/class/stock.ts
@@ -1,4 +1,5 @@
 import type { Ticker } from "../interface/ticker";
+import type { StockData } from "./stockData";
 
 export class Stock implements Ticker {
     ticket_name: string;
@@ -15,36 +16,23 @@ export class Stock implements Ticker {
     market_cap: number;
     div_yield: number;
 
-    constructor(
-        ticket_name: string,
-        price: number,
-        volume: number,
-        average_volume: number,
-        overnight_volume: number,
-        open: number,
-        today_high: number,
-        today_low: number,
-        week_high_52: number,
-        week_low_52: number,
-        pe_ratio: number,
-        market_cap: number,
-        div_yield: number){
-            this.ticket_name = ticket_name;
-            this.price = price
-            this.volume = volume
-            this.average_volume = average_volume
-            this.overnight_volume = overnight_volume
-            this.open = open
-            this.today_high = today_high
-            this.today_low = today_low
-            this.week_high_52 = week_high_52
-            this.week_low_52 = week_low_52
-            this.pe_ratio = pe_ratio
-            this.market_cap = market_cap
-            this.div_yield = div_yield
+    constructor(stockData: StockData){
+            this.ticket_name = stockData.ticket_name;
+            this.price = stockData.price
+            this.volume = stockData.volume
+            this.average_volume = stockData.average_volume
+            this.overnight_volume = stockData.overnight_volume
+            this.open = stockData.open
+            this.today_high = stockData.today_high
+            this.today_low = stockData.today_low
+            this.week_high_52 = stockData.week_high_52
+            this.week_low_52 = stockData.week_low_52
+            this.pe_ratio = stockData.pe_ratio
+            this.market_cap = stockData.market_cap
+            this.div_yield = stockData.div_yield
     }
 
-    updatePrice() {
+    private updatePrice() {
         this.price += 1
     }
 
