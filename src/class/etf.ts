862368bcbaf00e14ad8a43d13d412ae19fad9566
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-04-09 23:24:12 -0400

    Add etfData

--- src/class/etf.ts
+++ src/class/etf.ts
@@ -1,4 +1,5 @@
 import type { Ticker } from "../interface/ticker";
+import type { EtfData } from "./etfData";
 
 export class Etf implements Ticker {
     ticket_name: string;
@@ -16,35 +17,21 @@ export class Etf implements Ticker {
     expense_ratio: number;
     day_ratio_30: number;
 
-    constructor(
-        ticket_name: string,
-        price: number,
-        volume: number,
-        average_volume: number,
-        overnight_volume: number,
-        open: number,
-        today_high: number,
-        today_low: number,
-        week_high_52: number,
-        week_low_52: number,
-        pe_ratio: number,
-        aum: number,
-        expense_ratio: number,
-        day_ratio_30: number){
-            this.ticket_name = ticket_name
-            this.price = price
-            this.volume = volume
-            this.average_volume = average_volume
-            this.overnight_volume = overnight_volume
-            this.open = open
-            this.today_high = today_high
-            this.today_low = today_low
-            this.week_high_52 = week_high_52
-            this.week_low_52 = week_low_52
-            this.pe_ratio = pe_ratio
-            this.aum = aum
-            this.expense_ratio = expense_ratio
-            this.day_ratio_30 = day_ratio_30
+    constructor(etfData: EtfData){
+            this.ticket_name = etfData.ticket_name
+            this.price = etfData.price
+            this.volume = etfData.volume
+            this.average_volume = etfData.average_volume
+            this.overnight_volume = etfData.overnight_volume
+            this.open = etfData.open
+            this.today_high = etfData.today_high
+            this.today_low = etfData.today_low
+            this.week_high_52 = etfData.week_high_52
+            this.week_low_52 = etfData.week_low_52
+            this.pe_ratio = etfData.pe_ratio
+            this.aum = etfData.aum
+            this.expense_ratio = etfData.expense_ratio
+            this.day_ratio_30 = etfData.day_ratio_30
     }
 
     update(): void {
