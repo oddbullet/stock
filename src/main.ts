96e13615e1139dac9b54921a4525feb2c2c76f3b
Bill Yang <joyang12345@gmail.com>
2025-05-05 23:18:59 -0400

    setup apexchart

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueApexCharts from 'vue3-apexcharts';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueApexCharts);
+
+app.mount('#app');
