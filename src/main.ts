50e808e6d1a009c442a57a1ca681218d8e1ff32c
Bill Yang <joyang12345@gmail.com>
2025-05-16 00:18:31 -0400

    Added Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import VueApexCharts from 'vue3-apexcharts';
 
+const pinia = createPinia()
 const app = createApp(App);
-app.use(VueApexCharts);
+
+app.use(VueApexCharts, pinia);
 
 app.mount('#app');
